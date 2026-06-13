73e660629164180c2f530eb001146618e2024235
mukulhayaran <hayaranmukul@gmail.com>
2026-06-13 18:37:41 +0530

    sentiment analysis

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from transformers import pipeline
 
 app = FastAPI()
+
+classifier = pipeline("sentiment-analysis")
 
 class TextInput(BaseModel):
     text: str
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello from FastAPI!"}
+    return {"message": "Sentiment Analysis API"}
 
 @app.post("/predict")
 def predict(input: TextInput):
-    return {"you_sent": input.text, "length": len(input.text)}
+    result = classifier(input.text)
+    return {"label": result[0]["label"], "score": result[0]["score"]}
